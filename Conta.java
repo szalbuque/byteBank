9e8bc7b03559ffc6796bd146647cefd5ebc8924e
Silvia Rocha <szalbuque@gmail.com>
2022-09-14 15:59:29 -0300

    criados os getters e setters

--- Conta.java
+++ Conta.java
@@ -1,9 +1,36 @@
 public class Conta {
-    double saldo;
-    int agencia;
-    int numero;
-    Cliente titular;
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
 
+    public Cliente getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public int getAgencia(){
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo(){
+        return this.saldo;
+    }
     public void deposita(double valor){
         this.saldo += valor;
     }
--- CriaConta.java
+++ CriaConta.java
@@ -3,21 +3,24 @@ public class CriaConta {
         //primeiraConta é um ponteiro para um objeto do tipo Conta
 
         Conta primeiraConta = new Conta();
+        primeiraConta.setNumero(111);
+        primeiraConta.setAgencia(2222);
+
         Cliente primeiroCliente = new Cliente();
 
-        primeiroCliente.nome = "Silvia";
-        primeiroCliente.cpf = "111.111.111-11";
-        primeiroCliente.profissao = "Analista de sistemas";
-        primeiraConta.titular = primeiroCliente;
+        primeiroCliente.setNome("Silvia");
+        primeiroCliente.setCpf("111.111.111-11");
+        primeiroCliente.setProfissao("Analista de sistemas");
+        primeiraConta.setTitular(primeiroCliente);
 
-        System.out.println(primeiraConta.titular.nome);
+        System.out.println(primeiraConta.getTitular().getNome());
         primeiraConta.deposita(50);
-        System.out.println(primeiraConta.saldo);
+        System.out.println(primeiraConta.getSaldo());
         System.out.println(primeiraConta.saca(100));
-        System.out.println(primeiraConta.saldo);
+        System.out.println(primeiraConta.getSaldo());
 
         Conta contaDaSilvia = new Conta();
         primeiraConta.transfere(10, contaDaSilvia);
-        System.out.println(contaDaSilvia.saldo);
+        System.out.println(contaDaSilvia.getSaldo());
     }
 }
